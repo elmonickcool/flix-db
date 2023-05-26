8c5b34a95d83d3a0de6a48dd57b7892fe86868b1
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-05-27 00:54:17 +0800

    pre implement navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-
+import React from 'react';
+import Navbar from '../components/navbar';
 
 export default function Home() {
   return (
-   <div className="container">
-
-   </div>
-  )
+    <div>
+      <Navbar />
+     
+    </div>
+  );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,20 @@
-export default function Navbar(){
-    return(
-        <div className="navbar">
-            
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800">
+      <div className="container mx-auto px-5">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <a href="#" className="text-white text-lg font-semibold">FlixDB</a>
+          </div>
+          <div className="hidden md:flex md:space-x-4">
+            <a href="#home" className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Home</a>
+            <a href="#home" className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Movies</a>
+            <a href="#home" className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">TV Shows</a>
+          </div>
         </div>
-    )
+      </div>
+    </nav>
+  );
 }
