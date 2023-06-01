a852fc20481b5d1053ad9c2b3da255ca3c569274
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-06-01 23:50:13 +0800

    implement searchbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import SearchBar from '../components/searchbar'
 
 export default function Page() {
+  
   return (
-    <div>
-      Index Page
-     
+    <>
+    <div className="flex justify-center items-center ">
+    <h1 className="text-2xl font-bold">FlixDB</h1>
     </div>
+    <div className="flex justify-center items-center mt-4">
+        <SearchBar />
+      </div>
+    
+    </>
+    
   );
 }
---
+++ src/components/searchbar.tsx
@@ -0,0 +1,9 @@
+export default function SearchBar() {
+    return (
+      <form>
+        <input type="text" className="text-black" placeholder="Search a Movie" />
+        <input type="submit"/>
+      </form>
+    );
+  }
+  
