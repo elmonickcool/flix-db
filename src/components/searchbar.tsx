cb9007066240da6c43f43abb7c59e2b378d32001
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-06-06 18:12:48 +0800

    components to be change

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,9 +1,11 @@
 export default function SearchBar() {
     return (
+      <>
       <form>
-        <input type="text" className="text-black" placeholder="Search a Movie" />
-        <input type="submit"/>
+        <input type="text" className="form-input px-4 py-3 rounded-full text-black border" placeholder="Search a Movie" />
+        <input type="submit" className="rounded-full"/>
       </form>
+      </>
     );
   }
   
